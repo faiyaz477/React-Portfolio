59258d42652e0e1659507c7f432e02016ab95414
Faiyaz Alam <faiyaz48989@gmail.com>
2023-02-13 15:48:33 -0500

    update

---
+++ Develop/Portfolio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PortfolioContainer from './components/PortfolioContainer';
+
+function App() {
+  return (
+    <div >
+      < PortfolioContainer />
+    </div>
+  );
+}
+
+export default App;
